7d015554224657f58f79f4f1c02fe38774bc389a
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-09 10:38:43 -0400

    Create instagramprofile.js

---
+++ commands/fun/instagramprofile.js
@@ -0,0 +1,52 @@
+const {
+    VKontakte,
+    Instagram,
+    Facebook,
+    Snapchat,
+    Twitter,
+    YouTube,
+    TikTok
+} = require('social-downloader-cherry');
+
+let functions = require("../../functions.js")
+module.exports = {
+    name: "instagramprofile",
+    aliases: [],
+    category: "fun",
+    permission: [],
+    usage: ["$instagramprofile <username>"],
+    req_perms: ["SEND_MESSAGES"],
+    description: "get instagram user info",
+    run: async (client, message, args) => {
+
+        if (!args.join(" ")) return message.reply({
+            content: "Argument Missing: `instagram username`"
+        })
+        let username = args.join(" ")
+        const res = await Instagram.getStories(username)
+        if (res.data.hasError) return message.reply({
+            content: res.data.errorMessage
+        })
+      
+      let profile = res.data.body.profile
+      
+      let embed = {
+          title: `${profile.usernae}'s instagram`,
+          description: `Avatar - [link](${profile.profile_pic_url})`,
+          fields: [
+              {name: `Profile`, value: `**Full Name:** \`${profile.full_name}\`\n**Is Private?:** \`${(profile.is_private) ? "Yes" : "No"}\`\n**Followers:** \`${profile.edge_followed_by.count}\`\n**Following:** \`${profile.edge_follow.count}\`\n**Stories:** \`${profile.stories.length}\``, true}
+          ],
+          image: {
+              url: profile.profile_pic_url
+          },
+          author: {
+              name: `${profile.full_name}`,
+              url: profile.profile_pic_url
+          },
+          footer: {
+              text: `${profile.id}`
+          }
+      }
+      return message.channel.send({embeds: [embed]})
+    }
+}
