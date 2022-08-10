6b9e1c23ffe71ad2c4527b0a597a80ede7a27c1a
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-10 18:13:33 -0400

    Create texttospeech.js

---
+++ commands/fun/texttospeech.js
@@ -0,0 +1,59 @@
+let {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  AudioPlayerStatus,
+  NoSubscriberBehavior
+} = require('@discordjs/voice');
+let {
+  Client,
+  Message,
+  MessageEmbed
+} = require("discord.js");
+let {
+  getAudioUrl
+} = require("google-tts-api");
+let functions = require("../../functions.js")
+module.exports = {
+  name: "texttospeech",
+  aliases: ["tts"],
+  category: "fun",
+  permission: [],
+  usage: ["$texttospeech <text>"],
+  req_perms: ["SEND_MESSAGES"],
+  description: "convert text to speech",
+  run: async (client, message, args) => {
+
+    if (!args.join(" ")) return message.reply({
+      content: "Argument Missing: `text`"
+    })
+    let text = args.join(" ")
+    let voiceChannel = message.member.voice.channel;
+    if (text.length > 500) return message.reply("Argument Invalid: `text: i can only speak 500 words`");
+    if (!voiceChannel) return message.channel.send("Please join a voice channel first");
+
+    let audioUrl = await getAudioUrl(text, {
+            lang: "en",
+            slow: false,
+            host: 'https://translate.google.com',
+            timeout: 5000,
+        });
+
+        let player = createAudioPlayer();
+        let resource = createAudioResource(audioUrl);
+
+        let connection = joinVoiceChannel({
+            channelId: message.member.voice.channel.id,
+            guildId: message.channel.guild.id,
+            adapterCreator: message.channel.guild.voiceAdapterCreator,
+        });
+
+        player.play(resource);
+        connection.subscribe(player);
+
+        player.on(AudioPlayerStatus.Idle, () => {
+            connection.disconnect();
+        });
+
+  }
+}
