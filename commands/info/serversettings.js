e3fcd5914655fa94313780a10ff6312769d07a87
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-11 20:41:14 -0400

    Update serversettings.js

--- commands/info/serversettings.js
+++ commands/info/serversettings.js
@@ -1,4 +1,6 @@
-const { WelcomeScreen } = require("discord.js");
+const {
+  WelcomeScreen
+} = require("discord.js");
 const Discord = require("discord.js")
 const {
   MessageEmbed,
@@ -22,58 +24,69 @@ module.exports = {
     let gs = functions.cloneobj(client.guild_schema)
     gs._id = message.guild.id
     let gdb = require("../../Models/Guild")
-    
+
     let data = await functions.getdb(gdb, {
       _id: message.guild.id
     })
-    if(!data){
+    if(!data) {
       data = new gdb(gs)
       data.save()
     }
-    
-    if (!data || data.prefix == null) {
+
+    if(!data || data.prefix == null) {
       prefix = "$"
     } else {
       prefix = data.prefix
     }
     let jtc = message.guild.channels.cache.get(data.jtc)
-    if(!jtc){
+    if(!jtc) {
       jtc = "Not Set"
     } else {
       jtc = "#" + jtc.name
     }
     let whitelisted_role = message.guild.roles.cache.get(data.whitelistrole)
-    if(!whitelisted_role){
+    if(!whitelisted_role) {
       whitelisted_role = "Not Set"
     } else {
       whitelisted_role = whitelisted_role.name
     }
     let trusted_role = message.guild.roles.cache.get(data.trustrole)
-    if(!trusted_role){
+    if(!trusted_role) {
       trusted_role = "Not Set"
     } else {
       trusted_role = trusted_role.name
     }
     let quarantine_role = message.guild.roles.cache.get(data.quarantinerole)
-    if(!quarantine_role){
+    if(!quarantine_role) {
       quarantine_role = "Not Set"
     } else {
       quarantine_role = quarantine_role.name
     }
-    
+
     let welcome_channel = message.guild.channels.cache.get(data.welcome_channel)
-    if(!welcome_channel){
+    if(!welcome_channel) {
       welcome_channel = "Not Set"
     } else {
       welcome_channel = "#" + welcome_channel.name
     }
-let logs_channel = message.guild.channels.cache.get(data.logs_channel)
-    if(!logs_channel){
+    let leave_channel = message.guild.channels.cache.get(data.leave_channel)
+    if(!leave_channel) {
+      leave_channel = "Not Set"
+    } else {
+      leave_channel = "#" + leave_channel.name
+    }
+    let logs_channel = message.guild.channels.cache.get(data.logs_channel)
+    if(!logs_channel) {
       logs_channel = "Not Set"
     } else {
       logs_channel = "#" + logs_channel.name
     }
-    
+    let botlogs_channel = message.guild.channels.cache.get(data.bot_logs_channel)
+    if(!botlogs_channel) {
+      botlogs_channel = "Not Set"
+    } else {
+      botlogs_channel = "#" + botlogs_channel.name
+    }
     let embed = new MessageEmbed()
       .setTitle(`${message.guild.name} Custom Settings`)
       .setColor("DARK_BUT_NOT_BLACK")
@@ -81,8 +94,9 @@ let logs_channel = message.guild.channels.cache.get(data.logs_channel)
       .addField("PokeHelper", `\`\`\`${(data.pokehelper) ? "Enabled" : "Disabled"}\`\`\``, true)
       .addField("Join To Create", `\`\`\`${jtc}\`\`\``, true)
       .addField("Welcome Channel", `\`\`\`${welcome_channel}\`\`\``, true)
-      .addField("Welcome Message", `\`\`\`\n${JSON.stringify(data.welcome_message, null, 2)}}\`\`\``, true)
-      
+      .addField("Leave Channel", `\`\`\`${leave_channel}\`\`\``, true)
+      .addField("Logs Channel", `\`\`\`${logs_channel}\`\`\``, true)
+      .addField("Bot Logs Channel", `\`\`\`${botlogs_channel}\`\`\``, true)
       .addField("Auto Role", `\`\`\`${(data.auto_role) ? "Enabled" : "Disabled"}\`\`\``, true)
       .addField("Auto Roles", `\`\`\`${data.auto_roles.length}\`\`\``, true)
       .addField("Status Role", `\`\`\`${(data.status_role) ? "Enabled" : "Disabled"}\`\`\``, true)
