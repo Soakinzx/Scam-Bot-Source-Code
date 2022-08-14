bbc2a49f995ea5d6d40512b2e8d351c048f75a4b
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-13 20:17:56 -0400

    Create minecraftserver.js

---
+++ commands/fun/minecraftserver.js
@@ -0,0 +1,43 @@
+const request = require("request")
+const functions = require("../../functions.js")
+const Discord = require("discord.js")
+module.exports = {
+  name: "dict",
+  category: "fun",
+  aliases: ["dictionary"],
+  permission: [],
+  req_perms: ["SEND_MESSAGES"],
+  description: "search a word on the dictonary",
+  usage: ["$dict <word>"],
+  run: async (client, message, args) => {
+    const ip = args.join(" ")
+    const url = "https://mcapi.us/server/status?ip="+ip
+    request({
+    method: 'GET',
+    url: url
+}, (err, res, body) => {
+
+    if (err) return;
+
+    let info = JSON.parse(body)
+    const text = $('[value=1]').text() || "No definition found"
+    if(info.error !== null){
+        return message.reply({content: `${info.error}`})
+    } else {
+        let embed = {
+            title: `Minecraft Server Info`,
+            description: `${info.motd}`,
+            fields: [
+                {name: `Players`, value: `${functions.text_block(`${info.players.now}/${info.players.max}`)}`, inline: true},
+                {name: `Status`, value: `${functions.text_block(`${(info.online)?"Online":"Offline"}`)}`, inline: true}
+            ],
+            url: ip,
+            thumbnail: {
+                url: info.favicon
+            }
+    }
+    message.channel.send({embeds:[embed]})
+    }
+});
+},
+}
