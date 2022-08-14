adb7059eecd7a5de8c803ad84966d90d78e5a2ca
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-13 20:46:16 -0400

    Update minecraftserver.js

--- commands/fun/minecraftserver.js
+++ commands/fun/minecraftserver.js
@@ -10,34 +10,48 @@ module.exports = {
   description: "get info on a minecraft server",
   usage: ["$minecraftserver <server ip>"],
   run: async (client, message, args) => {
-    const ip = args.join(" ")
-    const url = "https://mcapi.us/server/status?ip="+ip
+    const ip = args[0]
+    if(!ip) return message.reply({content: "Argument Missing: `server ip`"})
+    const url = "https://eu.mc-api.net/v3/server/ping/" + ip
     request({
-    method: 'GET',
-    url: url
-}, (err, res, body) => {
-
-    if (err) return;
-
-    let info = JSON.parse(body)
-    const text = $('[value=1]').text() || "No definition found"
-    if(info.error !== null){
-        return message.reply({content: `${info.error}`})
-    } else {
+      method: 'GET',
+      url: url
+    }, (err, res, body) => {
+      if(err) return;
+      
+      
+      if(res.statusCode !== 200) {
+        return message.reply({
+          content: `Could Not Ping Server \`${ip}\` With Error Code: ${res.statusCode}`
+        })
+      } else {
+        let info = JSON.parse(body)
         let embed = {
-            title: `Minecraft Server Info`,
-            description: `${info.motd}`,
-            fields: [
-                {name: `Players`, value: `${functions.text_block(`${info.players.now}/${info.players.max}`)}`, inline: true},
-                {name: `Status`, value: `${functions.text_block(`${(info.online)?"Online":"Offline"}`)}`, inline: true}
-            ],
-            url: ip,
-            thumbnail: {
-                url: info.favicon
+          title: `Minecraft Server Info`,
+          description: `${info.description}`,
+          fields: [{
+              name: `Players`,
+              value: `${functions.text_block(`${info.players.online}/${info.players.max}`)}`,
+              inline: true
+            },
+            {
+              name: `Status`,
+              value: `${functions.text_block(`${(info.online)?"Online":"Offline"}`)}`,
+              inline: true
             }
-    }
-    message.channel.send({embeds:[embed]})
-    }
-});
-},
+          ],
+          url: "https://"+ip,
+          thumbnail: {
+            url: info.favicon
+          },
+          footer: {
+              text: `Took ${info.took}ms to get minecraft server`
+          }
+        }
+        message.channel.send({
+          embeds: [embed]
+        })
+      }
+    });
+  },
 }
