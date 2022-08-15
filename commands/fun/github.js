da6ed8db73ad6cde47729960f4e7d45bb154e88e
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-15 19:49:41 -0400

    Update github.js

--- commands/fun/github.js
+++ commands/fun/github.js
@@ -10,28 +10,32 @@ module.exports = {
   req_perms: ["SEND_MESSAGES"],
   description: "get info on a github user",
   run: async (client, message, args) => {
-
-
+    if(!args.length) return message.reply({
+      content: "Argument Missing: `user`"
+    })
     request(`https://api.popcat.xyz/github/${args.join(" ")}`, async (err, res, body) => {
       if(err) return;
       let json = JSON.parse(body)
       let embed = {
-          title: "Github"
+        title: "Github"
       }
       if(json.error) {
-          embed.description = json.error
+        embed.description = json.error
       } else {
-          embed.description = json.bio
-          embed.fields = [
-              {name: "Info", value: `**Name:** \`${json.name}\`\n**Account Type:** \`${json.account_type}\`\n**Blog:** \`${json.blog}\`\n**Company:** \`${json.company}\`\n**Email:** \`${json.email}\`\n**Location:** \`${json.location}\`\n**Twitter:** \`${json.twitter}\`\n**Public Repos:** \`${json.public_repos}\`\n**Public Gists:** \`${json.public_gists}\`\n**Followers:** \`${json.followers}\`\n**Following:** \`${json.following}\`\n**Created At:** \`${json.created_at}\`\n**Updated At:** \`${json.updated_at}\``, inline: true}
-          ]
-          embed.thumbnail = {
-              url: json.avatar
-          }
-          embed.url = json.url
+        embed.description = json.bio
+        embed.fields = [{
+          name: "Info",
+          value: `**Name:** \`${json.name}\`\n**Account Type:** \`${json.account_type}\`\n**Blog:** \`${json.blog}\`\n**Company:** \`${json.company}\`\n**Email:** \`${json.email}\`\n**Location:** \`${json.location}\`\n**Twitter:** \`${json.twitter}\`\n**Public Repos:** \`${json.public_repos}\`\n**Public Gists:** \`${json.public_gists}\`\n**Followers:** \`${json.followers}\`\n**Following:** \`${json.following}\`\n**Created At:** \`${json.created_at}\`\n**Updated At:** \`${json.updated_at}\``,
+          inline: true
+        }]
+        embed.thumbnail = {
+          url: json.avatar
+        }
+        embed.url = json.url
       }
-      return message.channel.send({embeds: [embed]})
+      return message.channel.send({
+        embeds: [embed]
+      })
     })
-
   }
 }
