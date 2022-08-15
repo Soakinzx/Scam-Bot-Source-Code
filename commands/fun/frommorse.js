d6d8ffa504f303daec1bae91da7a612d7a6aba78
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-15 18:42:55 -0400

    Create frommorse.js

---
+++ commands/fun/frommorse.js
@@ -0,0 +1,84 @@
+const functions = require("../../functions.js")
+let fs = require("fs")
+module.exports = {
+  name: "frommorse",
+  permission: [],
+  aliases: [],
+  category: "fun",
+  req_perms: ["SEND_MESSAGES"],
+  usage: ["$frommorse <morse code>"],
+  description: "Make any morse code into text",
+
+  run: async (client, message, args) => {
+    if(!args.length) return message.reply({
+      content: "Argument Missing: `morse code`"
+    })
+    let morse_letters = {
+                ".-": "A",
+                "-...": "B",
+                "-.-.": "C",
+                "-..": "D",
+                ".": "E",
+                "..-.": "F",
+                "--.": "G",
+                "....": "H",
+                "..": "I",
+                ".---": "J",
+                "-.-": "K",
+                ".-..": "L",
+                "--": "M",
+                "-.": "N",
+                "---": "O",
+                ".--.": "P",
+                "--.-": "Q",
+                ".-.": "R",
+                "...": "S",
+                "-": "T",
+                "..-": "U",
+                "...-": "V",
+                ".--": "W",
+                "-..-": "X",
+                "-.--": "Y",
+                "--..": "Z",
+                "-----": "0",
+                ".----": "1",
+                "..---": "2",
+                "...--": "3",
+                "....-": "4",
+                ".....": "5",
+                "-....": "6",
+                "--...": "7",
+                "---..": "8",
+                "----.": "9"
+            };
+
+    function morseletter2Text(t) {
+      let morse = "";
+      for(i = 0; i < t.length; i++) {
+        if(morse_letters[t[i]]) {
+            morse += morse_letters(t[i]) + " ";
+        } else {
+            morse += t[i]+" "
+        }
+      }
+      return morse
+    }
+    let text = functions.text_block(morseletter2Text(args.join(" ")))
+    if(text.length <= 4000) {
+      return message.reply({
+        content: text
+      })
+    } else {
+      let txt = `././txt/${message.member.id}_morsecode_to_text.txt`
+      await fs.writeFileSync(txt, text)
+
+      await message.reply({
+        content: `Morse Code To Text Too Long Created A .txt File`,
+        files: [txt]
+      })
+      fs.unlinkSync(txt, (err) => {
+        console.log(err)
+      })
+    }
+  },
+};
