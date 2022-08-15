ca2390be60fc89c09f7483194d342fcc4317feba
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-15 19:57:22 -0400

    Create randomcolor.js

---
+++ commands/fun/randomcolor.js
@@ -0,0 +1,28 @@
+const discord = require("discord.js")
+let functions = require("../../functions.js")
+const request = require("request")
+module.exports = {
+  name: "randomcolor",
+  aliases: [],
+  category: "fun",
+  permission: [],
+  usage: ["$randomcolor"],
+  req_perms: ["SEND_MESSAGES"],
+  description: "get a random color",
+  run: async (client, message, args) => {
+
+
+    request("https://api.popcat.xyz/randomcolor", async (err, res, body) => {
+      if(err) return;
+      let json = JSON.parse(body)
+      let embed = new discord.MessageEmbed
+      embed.setColor(json.hex)
+      embed.setTitle(`${json.name} | ${json.text}`)
+      embed.setImage(json.image)
+      message.channel.send({
+        embeds: [embed]
+      })
+    })
+
+  }
+}
