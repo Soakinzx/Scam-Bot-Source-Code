1d144cf2cb9e036c790de0c5942f9311eeaf7e80
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-16 23:59:59 -0400

    Update createrole.js

--- commands/moderation/createrole.js
+++ commands/moderation/createrole.js
@@ -3,30 +3,40 @@ const {
   MessageEmbed
 } = require("discord.js");
 module.exports = {
-  name: "createrole",
+  name: "deleterole",
   aliases: [],
   permission: ["MANAGE_ROLES"],
   category: "moderation",
   req_perms: ["MANAGE_ROLES", "SEND_MESSAGES"],
-  usage: ["$createrole <name>"],
-  description: "create a role",
+  usage: ["$deleterole <role>"],
+  description: "delete a role",
   run: async (client, message, args) => {
-    let role_name = args.join(" ")
-    
-    if(!role_name) return message.reply({
-      content: "Argument Missing: `role name`"
+    if(!args.join(" ")) return message.reply({
+      content: "Argument Missing: `@role`"
     })
-    if(role_name.length > 32) return message.reply({
-      content: "Argument Invalid: `role name: must be less than or equal to 32 characters`"
+    let role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]) || message.guild.roles.cache.find(r => r.name.toLowerCase().startsWith(args.join(" ").toLowerCase()))
+    if(!role) return message.reply({
+      content: "Argument Invalid: `@role`"
     })
-    message.guild.roles.create({
-         name: role_name,
-         reason: `created by ${message.author.tag}`
-     }).then(async() => {
-         return message.reply({content: `Successfully created a role with the name \`${role_name}\``})
-     }).catch(err => {
-         return message.reply({content: `${err}`})
-     })
-    
+    if(message.member.roles.highest.comparePositionTo(role) <= 0 && message.guild.ownerId !== message.member.id) return message.reply({
+      content: `\`${role.name}\` role has higher/equal authority than your current highest role`
+    })
+
+    if(message.guild.me.roles.highest.comparePositionTo(role) < 0) return message.reply({
+      content: `\`${role.name}\` role has higher authority than my current highest role`
+    })
+
+    message.guild.roles.delete(role.id, `deleted by ${message.author.tag}`)
+      .then(async () => {
+        return message.reply({
+          content: `Successfully deleted a role with the name \`${role.name}\``
+        })
+      })
+      .catch(err => {
+        return message.reply({
+          content: `${err}`
+        })
+      })
+
   },
 }
