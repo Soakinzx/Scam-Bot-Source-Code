852edaf50987cbcff136b8c776817ceab2924b55
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-18 18:18:12 -0400

    Update messageCreate.js

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -127,15 +127,15 @@ module.exports = {
               missing_perms.push(command.permission[i])
             }
           } else if(command.permission[i] == "SERVER_TRUSTED") {
-            if(!guild_data){
+            if(!guild_data && message.member.id !== message.guild.ownerId){
               missing_perms.push(command.permission[i])
-            } else if(message.member.id !== message.guild.ownerId && !message.member.roles.cache.has(guild_data.trustrole) && !guild_data.trusted.includes(message.member.id) && message.member.id !== message.guild.ownerId) {
+            } else if(message.member.id !== message.guild.ownerId && !message.member.roles.cache.has(guild_data.trustrole) && !guild_data.trusted.includes(message.member.id)) {
               missing_perms.push(command.permission[i])
             }
           } else if(command.permission[i] == "SERVER_WHITELISTED") {
-            if(!guild_data){
+            if(!guild_data && message.member.id !== message.guild.ownerId){
               missing_perms.push(command.permission[i])
-            } else if(message.member.id !== message.guild.ownerId && !message.member.roles.cache.has(guild_data.trustrole) && !guild_data.trusted.includes(message.member.id) && !guild_data.whitelisted.includes(message.member.id) && !message.member.roles.cache.has(guild_data.whitelistrole) && message.member.id !== message.guild.ownerId) {
+            } else if(message.member.id !== message.guild.ownerId && !message.member.roles.cache.has(guild_data.trustrole) && !guild_data.trusted.includes(message.member.id) && !guild_data.whitelisted.includes(message.member.id) && !message.member.roles.cache.has(guild_data.whitelistrole)) {
               missing_perms.push(command.permission[i])
             }
           } else {
