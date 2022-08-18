d845e9cd2c65b912513dd036504dff259249d507
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-17 21:04:55 -0400

    Update afk.js

--- Events/afk.js
+++ Events/afk.js
@@ -21,12 +21,13 @@ module.exports = {
       _id: msg.guild.id
     })
     if(!data || data.afk_list.length == 0) return
-    if(mentions.size >= 1) {
-      for(const member of mentions) {
-        if(data.afk_list.find(afk_user => afk_user.id == member.id)) {
+    if(mentions.size > 0) {
+      mentions.forEach(member => {
+        let i = data.afk_list.find(afk_user => afk_user.id == member.id)
+        if(i) {
           let embed = new Discord.MessageEmbed()
             .setTitle("AFK")
-            .setDescription(`**${messageMember} is currently AFK:** ${i.message}`)
+            .setDescription(`**${member} is currently AFK:** ${i.message}`)
             .setFooter(moment(i.date)
               .fromNow())
             .setColor("DARK_BUT_NOT_BLACK")
@@ -34,30 +35,30 @@ module.exports = {
             embeds: [embed]
           })
         }
-      }
-    }
-    let check = data.afk_list.find(afk_user => afk_user.id == msg.author.id)
-    if(check) {
-      data.afk_list.splice(data.afk_list.indexOf(check), 1)
-      data.save()
-      let embed = new Discord.MessageEmbed()
-        .setTitle("AFK REMOVED")
-        .setDescription(`${msg.author} Successfully removed your afk status!`)
-        .setColor("DARK_BUT_NOT_BLACK")
-        .setTimestamp()
-      try {
-        msg.member.setNickname(msg.member.nickname.replace("[AFK]", ""))
-          .catch(err => {
-            return
-          })
-      } catch (err) {
-        return;
-      }
-      msg.reply({
-        embeds: [embed]
       })
     }
-
+    for(const i of data.afk_list) {
+      if(i.id == msg.member.id) {
+        data.afk_list.splice(data.afk_list.indexOf(i),1)
+        data.save()
+        let embed = new Discord.MessageEmbed()
+          .setTitle("AFK REMOVED")
+          .setDescription(`${msg.author} Successfully removed your afk status!`)
+          .setColor("DARK_BUT_NOT_BLACK")
+          .setTimestamp()
+        try {
+          msg.member.setNickname(msg.member.nickname.replace("[AFK]", ""))
+            .catch(err => {
+              let i=0
+            })
+        } catch (err) {
+          let i=0
+        }
+        msg.reply({
+          embeds: [embed]
+        })
+      }
+    }
 
   },
 };
