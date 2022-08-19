3b2ce9d86af7dfbc7948d2fec42a96b492c32622
Soakinzx <86316089+Soakinzx@users.noreply.github.com>
2022-08-19 14:46:30 -0400

    Update youtubecomment.js

--- commands/image/youtubecomment.js
+++ commands/image/youtubecomment.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const axios = require("axios")
 module.exports = {
     name: "youtubecomment",
-    aliases: ["yt"],
+    aliases: ["ytcomment"],
     permission: [],
     category: "image",
     req_perms: ["SEND_MESSAGES"],
@@ -10,10 +10,10 @@ module.exports = {
     usage: ["$youtubecomment <optional: user> <text>"],
     run: async (client, message, args) => {
         let user = message.author
-        let text = args.join(" ")
+        let text = args.join("%20")
         if(message.mentions.users.first() && args[0] && args[0].includes(message.mentions.users.first().id)){
             user = message.mentions.users.first()
-            text = args.splice(1).join(" ")
+            text = args.splice(1).join("%20")
         }
         
         
@@ -21,7 +21,7 @@ module.exports = {
         if(!text) return message.reply({content: "Argument Missing: `text`"})
         
         
-        let url = encodeURI(`https://some-random-api.ml/canvas/youtube-comment?username=${user.username}&avatar=${user.displayAvatarURL()}&comment=${text}`)
+        let url = `https://some-random-api.ml/canvas/youtube-comment?username=${user.username.replaceAll(" ", "%20")}&avatar=${user.displayAvatarURL().replace(".webp", ".jpg")}&comment=${text}`
         message.channel.send({embeds: [{
             title: "Youtube Comment",
             image: {
